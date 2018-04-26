6a61b7da31853d4031914c8f4cbb59209cfccf26
erikor <ericjkort@gmail.com>
2018-04-26 11:51:58 -0400

    Update stat_compare_means.R

--- R/stat_compare_means.R
+++ R/stat_compare_means.R
@@ -99,7 +99,7 @@ stat_compare_means <- function(mapping = NULL, data = NULL,
                      comparisons = NULL, hide.ns = FALSE, label.sep = ", ",
                      label = NULL, label.x.npc = "left", label.y.npc = "top",
                      label.x = NULL, label.y = NULL, tip.length = 0.03,
-                     symnum.args = list(),
+                     symnum.args = list(), textsize=2
                      geom = "text", position = "identity",  na.rm = FALSE, show.legend = NA,
                     inherit.aes = TRUE, ...) {
 
@@ -142,7 +142,7 @@ stat_compare_means <- function(mapping = NULL, data = NULL,
                           test = method, test.args = method.args,
                           step_increase = step_increase, size = size, color = color,
                           map_signif_level = map_signif_level, position = position, tip_length = tip.length,
-                          data = data, ...)
+                          data = data, textsize=textsize)
   }
 
   else{
